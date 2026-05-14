41da3c68d9103e3b75268a54c5078dd9ef2e67b4
Diego Delvig <157409019+DiegoDelvig@users.noreply.github.com>
2026-05-14 18:27:02 +0200

    Add files via upload

--- modification.php
+++ modification.php
@@ -1,33 +1,56 @@
 <?php
-    error_reporting(0);
-    $client=json_decode($_COOKIE["client"], true);
-    if(!isset($_COOKIE["client"])){
-        header("Location: index.php");
-    }    
-    if(empty($_REQUEST['nname'])||empty($_REQUEST['nfname'])||empty($_REQUEST['nadr'])||empty($_REQUEST['ntel'])){
+error_reporting(0);
+$client = json_decode($_COOKIE["client"], true);
+if (!isset($_COOKIE["client"])) {
+    header("Location: index.php");
+}
+if (
+    empty($_REQUEST["nname"]) ||
+    empty($_REQUEST["nfname"]) ||
+    empty($_REQUEST["nadr"]) ||
+    empty($_REQUEST["ntel"])
+) {
+} else {
+    $file = "donnees/data.json";
+    if (file_exists($file)) {
+        $client_passe = file_get_contents($file);
+        $data = json_decode($client_passe, true);
     }
-    else{
-        $file= "donnees/data.json";
-        if(file_exists($file)){
-            $client_passe=file_get_contents($file);
-            $data=json_decode($client_passe, true);
-        }
-        $name=$_REQUEST['nname'];
-        $fname=$_REQUEST['nfname'];
-        $adr=$_REQUEST['nadr'];
-        $tel=$_REQUEST['ntel'];
-        $infocomp=$_REQUEST['ninfocomp'];
-        $new_user=array('name' => $name, 'fname'=>$fname, 'adr'=>$adr, 'tel'=>$tel, 'infocomp'=>$infocomp, 'email'=>$client['email'], 'code'=>$client['code'], 'point_fidelite'=>$client['point_fidelite'], 'numero_fidelite'=>$client['numero_fidelite'], 'role'=>array('livreur'=>$client['role']['livreur'],'admin'=>$client['role']['admin'],'bloque'=>$client['role']['bloque'], 'restaurateur'=>$client['role']['restaurateur']));
-        $data[$client['email']]=$new_user;
-        file_put_contents("donnees/data.json", json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-        setcookie("client", json_encode($data[$client['email']]), time()-3600);  
-        setcookie("client", json_encode($data[$client['email']]), time()+3600);
-        header("Location: profil.php");
-        exit;
-    }
+    $name = $_REQUEST["nname"];
+    $fname = $_REQUEST["nfname"];
+    $adr = $_REQUEST["nadr"];
+    $tel = $_REQUEST["ntel"];
+    $infocomp = $_REQUEST["ninfocomp"];
+    $new_user = [
+        "name" => $name,
+        "fname" => $fname,
+        "adr" => $adr,
+        "tel" => $tel,
+        "infocomp" => $infocomp,
+        "email" => $client["email"],
+        "code" => $client["code"],
+        "point_fidelite" => $client["point_fidelite"],
+        "numero_fidelite" => $client["numero_fidelite"],
+        "role" => [
+            "livreur" => $client["role"]["livreur"],
+            "admin" => $client["role"]["admin"],
+            "bloque" => $client["role"]["bloque"],
+            "restaurateur" => $client["role"]["restaurateur"],
+        ],
+    ];
+    $data[$client["email"]] = $new_user;
+    file_put_contents(
+        "donnees/data.json",
+        json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
+    );
+    setcookie("client", json_encode($data[$client["email"]]), time() - 3600);
+    setcookie("client", json_encode($data[$client["email"]]), time() + 3600);
+    header("Location: profil.php");
+    exit();
+}
 ?>
 <!DOCTYPE html>
-<html> 
+<html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -36,13 +59,13 @@
         <link rel="stylesheet" href="css/modification.css">
         <link href="assets/Logo projet.png" rel="icon">
         <title>Inscription - Les Croquettes du Chef</title>
-        
+
     </head>
     <body>
         <header>
         <div class="logo">
             <img src="assets/Logo projet.png" alt="Logo" class="header-logo">
-            Les croquettes du chef 
+            Les croquettes du chef
         </div>
         <nav>
             <ul>
@@ -54,17 +77,38 @@
         </nav>
         </header>
         <h1>MODIFICATION DE COMPTE</h1>
-        <form action="" method="post" target="_top" id="formulaire" name="connexion">
-            <div class="rect_bleu">
-                <img class="logo_login" src="assets/Logo projet.png" alt="logo de notre site de vente">
-                <input type="text" name="nname" id="idname" class="login_case_name" value="<?php  echo $client['name'];?>">
-                <input type="text" name="nfname" id="idfname" class="login_case_fname" value="<?php echo $client['fname']; ?>">
-                <input type="text" name="nadr" id="idadr" class="login_case_adr" value="<?php echo $client['adr']; ?>">
-                <input type="tel" name="ntel" id="idtel" class="login_case_tel" value="<?php echo $client['tel']; ?>">
-                <textarea name="ninfocomp" id="idinfocomp" class="login_case_infocomp"><?php echo $client['infocomp']; ?></textarea>
-                <input type="submit" value="Valider" class="login_submit">
-            </div>
-        </form>
+                <form action="" method="post" target="_top" id="formulaire" name="connexion">
+                    <div class="rect_bleu">
+                        <img class="logo_login" src="assets/Logo projet.png" alt="logo de notre site de vente">
+
+                        <label for="idname" class="input_label">Nom :</label>
+                        <input type="text" name="nname" id="idname" class="login_case_name" value="<?php echo $client[
+                            "name"
+                        ]; ?>">
+
+                        <label for="idfname" class="input_label">Prénom :</label>
+                        <input type="text" name="nfname" id="idfname" class="login_case_fname" value="<?php echo $client[
+                            "fname"
+                        ]; ?>">
+
+                        <label for="idadr" class="input_label">Adresse postale :</label>
+                        <input type="text" name="nadr" id="idadr" class="login_case_adr" value="<?php echo $client[
+                            "adr"
+                        ]; ?>">
+
+                        <label for="idtel" class="input_label">Numéro de téléphone :</label>
+                        <input type="tel" name="ntel" id="idtel" class="login_case_tel" value="<?php echo $client[
+                            "tel"
+                        ]; ?>">
+
+                        <label for="idinfocomp" class="input_label">Informations complémentaires :</label>
+                        <textarea name="ninfocomp" id="idinfocomp" class="login_case_infocomp"><?php echo $client[
+                            "infocomp"
+                        ]; ?></textarea>
+
+                        <input type="submit" value="Valider" class="login_submit">
+                    </div>
+                </form>
     <footer>
         <p>&copy; 2026 Les Croquettes du Chef - Espace Client</p>
     </footer>
