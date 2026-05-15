95dccf878ddce870dfdbb03d416175fd17dd85e1
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 12:24:20 +0200

    Refactor login.php for improved structure and clarity

--- login.php
+++ login.php
@@ -45,11 +45,12 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/variables.css">
+        <link id="theme-css" rel="stylesheet" href="css/variables.css">
         <link rel="stylesheet" href="css/login.css">
         <link rel="stylesheet" href="css/client.css">
         <title>Connexion - Les Croquettes du Chef</title>
         <link href="assets/Logo projet.png" rel="icon">
+        <script src="js/charte.js"></script>
     </head>
     <body>
         <header>
