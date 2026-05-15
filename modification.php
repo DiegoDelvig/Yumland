408517bdc357aefc8f3555396f0e3e5768a86373
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 12:01:34 +0200

    Update modification.php

--- modification.php
+++ modification.php
@@ -73,6 +73,7 @@ if (
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="menu.php">La Carte</a></li>
                 <li><a href="inscription.php" class="active">Modifaction de Compte</a></li>
+                <li><button id="btn-theme" onclick="changerTheme();">🌙</button></li>
                 <li><a href="profil.php" class="btn">Profil</a></li>
             </ul>
         </nav>
