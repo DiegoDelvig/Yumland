e959882167ec9fb4411c14e820d3e467319e6bba
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 11:31:20 +0200

    Add theme CSS link and script to confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -77,10 +77,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commande Confirmée - Les Croquettes du Chef</title>
-    <link rel="stylesheet" href="css/variables.css">
+    <link id="theme-css" rel="stylesheet" href="css/variables.css">
     <link rel="stylesheet" href="css/client.css">
     <link rel="stylesheet" href="css/confirmation.css">
     <link href="assets/Logo projet.png" rel="icon">
+    <script src="js/charte.js></script>
 </head>
 <body>
     <header>
