ab39bf9cd56521515e8124350d396a1c8cde9d5a
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 11:33:08 +0200

    Add theme CSS link to livraisons.php

--- livraisons.php
+++ livraisons.php
@@ -110,7 +110,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livraison - Les Croquettes du Chef</title>
-    <link rel="stylesheet" href="css/variables.css">
+    <link id="theme-css" rel="stylesheet" href="css/variables.css">
     <link rel="stylesheet" href="css/client.css">
     <link rel="stylesheet" href="css/livraisons.css">
     <link href="assets/Logo projet.png" rel="icon">
