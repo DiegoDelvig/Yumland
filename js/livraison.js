0deb01bf2ee0c639bc98c238ef35d87f0b392626
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 11:13:18 +0200

    Add livrerCommande function for order delivery

--- js/livraison.js
+++ js/livraison.js
@@ -1,1 +1,23 @@
-test
+function livrerCommande() {
+    window
+        .fetch(new Request("livrer_commande.php"))
+
+        .then((reponse) => {
+            if (reponse == null) {
+                document.getElementById("message-livraison").innerHTML = "Erreur, réessayez.";
+                document.getElementById("message-livraison").classList.remove("cache");
+            }
+            else return reponse.json();
+        })
+
+        .then((reponse) => {
+            if (reponse != null && reponse["succes"] == true) {
+                document.querySelector(".btn-livraison").classList.add("cache");
+                document.getElementById("message-livraison").classList.remove("cache");
+            }
+        })
+
+        .catch((error) => {
+            console.log("Erreur : " + error);
+        });
+}
