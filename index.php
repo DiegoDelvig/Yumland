268a1fca966af2ee2cc5e9bacb04062a408a3494
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 11:31:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les Croquettes du Chef - Pour Chiens</title>
     
-    <link rel="stylesheet" href="css/variables.css">
+    <link id="theme-css" rel="stylesheet" href="css/variables.css">
     <link rel="stylesheet" href="css/client.css">
     <link rel="stylesheet" href="css/accueil.css">
     <link href="assets/Logo projet.png" rel="icon">
+    <script src="js/charte.js"></script>
 </head>
 <body>
 
