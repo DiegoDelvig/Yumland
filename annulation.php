0da80789263137efb9f790548e7f22a1f2c1c00d
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 11:30:38 +0200

    Update annulation.php

--- annulation.php
+++ annulation.php
@@ -64,10 +64,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Commande Refusée - Les Croquettes du Chef</title>
-    <link rel="stylesheet" href="css/variables.css">
+    <link id="theme-css" rel="stylesheet" href="css/variables.css">
     <link rel="stylesheet" href="css/client.css">
     <link rel="stylesheet" href="css/confirmation.css">
     <link href="assets/Logo projet.png" rel="icon">
+    <script src="js/charte.js"></script>
 </head>
 <body>
     <header>
