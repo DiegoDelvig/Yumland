06e9d53fdd45f8605b1ea770776b6864f9e3706e
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-15 11:34:51 +0200

    Add theme CSS link and script to modification.php

--- modification.php
+++ modification.php
@@ -54,10 +54,11 @@ if (
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/variables.css">
+        <link id="theme-css" rel="stylesheet" href="css/variables.css">
         <link rel="stylesheet" href="css/client.css">
         <link rel="stylesheet" href="css/modification.css">
         <link href="assets/Logo projet.png" rel="icon">
+        <script src="js/charte.js></script>
         <title>Inscription - Les Croquettes du Chef</title>
 
     </head>
