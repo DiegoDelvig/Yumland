5593c73db866d1c47cb1effb873e1d2cabc1ed3a
Imane Bateoui <e-ibateoui1@etu.cyu.fr>
2026-05-16 21:56:38 +0200

    Refactor login.php for improved readability

--- login.php
+++ login.php
@@ -50,7 +50,7 @@
         <link rel="stylesheet" href="css/client.css">
         <title>Connexion - Les Croquettes du Chef</title>
         <link href="assets/Logo projet.png" rel="icon">
-        <script src="js/charte.js"></script>
+        <script src="js/charte.js" defer></script>
         <script src="js/validation.js" defer></script>
         <script src="js/login.js" defer></script>
     </head>
